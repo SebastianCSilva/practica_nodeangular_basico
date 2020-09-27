ff7563ca216da8805ddeef489061183a93e29863
sebastian.cornejo.silva@gmail.com <sebastian.cornejo.silva@gmail.com>
2020-09-27 04:32:32 -0300

    se agrega la ruta singin con sus propias validaciones y que devuelve un token

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,6 +4,8 @@ const userSchema = new Schema({
     email: String,
     password: String
 
+}, {
+    timestamps: true
 });
 
 module.exports = model('User', userSchema);
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,12 +3,32 @@ const router = Router();
 
 const User = require('../models/User');
 
+const jwt = require('jsonwebtoken');
+
+
+
 router.get('/', (req, res) => res.send('Hello world'))
 
-router.post('/signup', (req, res) => {
-    res.send('testing signup');
+router.post('/signup', async (req, res) => {
+    const { email, password} = req.body;
+    const newUser = new User({email, password});
+    await newUser.save();
+    
+    const token = jwt.sign({_id: newUser._id }, 'secretKey')
+    res.status(200).json({token})
 
 })
+
+router.post('/signin', async (req, res) => {
+    const { email, password} =req.body;
+    const user = await User.findOne({email})
+    if (!user) return res.status(401).send("The email doesn't exists");
+    if (user.password !== password) return res.status(401).send("Wrong Password");
+
+    const token = jwt.sign({_id: user._id}, 'secretKey');
+    return res.status(200).json({token});
+
+});
 
 
 module.exports = router;
